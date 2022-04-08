ffb8ebc4946395487401fadc2c12b3120733b6bf
bft7658 <bft7658@gmail.com>
2022-04-08 22:48:25 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
+// 載入 mongoose
+const mongoose = require('mongoose')
+// 載入 Todo model
+const Todo = require('./models/todo')
+
 const app = express()
 const port = 3000
 
@@ -21,7 +25,10 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(port, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
-const Todo =require('../todo')
+const Todo = require('../todo')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('mongodb error!')
+  console.log('Connection error!')
 })
 
 db.once('open', () => {
-  console.log('mongodb connected!')
+  console.log('Connected to MongoDB!')
 
   for (let i = 0; i < 10; i++) {
-    Todo.create({name:`name-${i}`})
+    Todo.create({ name: `name-${i}` })
   }
 
   console.log('done')
