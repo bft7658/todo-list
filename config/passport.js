fac13b9173e50599a8fe85a9f9e1bd4eefeb53ce
bft7658 <bft7658@gmail.com>
2022-05-10 17:55:35 +0800

    feat: add warning message on login page

--- config/passport.js
+++ config/passport.js
@@ -9,17 +9,18 @@ module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
+  
   // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, { type: 'warning_msg', message: '信箱還未被註冊!' })
         }
         // 第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, { type: 'warning_msg', message: '信箱或密碼不正確!' })
           }
           return done(null, user)
         })
